b32519c2984b5e2a6b399a774dd31a8e9a1fedbe
Brucebe <bebert@udesa.edu.ar>
2023-04-12 00:46:54 -0300

    not found con css

---
+++ pro3/src/screens/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './notFound.css'
+
+function NotFound(props){
+
+    return(
+        <div className="notFound">
+            <img className="notFoundImg" src="../../img/notFound.png"></img>
+        </div>
+    )
+}
+
+export default NotFound;
